560bebf0438bf3b114f561c6bcc19bfc383922c9
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2016-10-10 23:50:34 -0400

    Add backend recaptcha verification

--- contact.php
+++ contact.php
@@ -7,7 +7,28 @@
         $subject = str_replace(array("\r","\n"),array(" "," "), strip_tags(trim($_POST["subject"])));
 
 
-        if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        # Recaptcha verification
+        $content = [
+            'secret' => '6Lf1xQgUAAAAABRD04M61uerCod3xW9jp-RfKlgV',
+            'response' => $_POST['g-recaptcha-response'],
+            'remoteip' => $_SERVER['REMOTE_ADDR']
+        ];
+
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+
+        $opts = [
+            'http' => [
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($content) 
+            ]
+        ];
+
+        $context = stream_context_create($opts);
+        $response = file_get_contents($url, false, $context);
+        $captcha = json_decode($response)->success;
+
+        if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR !$captcha) {
             http_response_code(400);
             echo "Looks like something wasn't quite right. Make sure you use a valid email address and fill in all the required fields!";
             exit;
